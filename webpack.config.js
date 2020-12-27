3cc5798be62560429f45843681e38fb6e7a22d8a
Nadiia <npodufala15@gmail.com>
2020-12-27 15:53:52 +0200

    clear html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 let path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/app.js',
@@ -13,5 +15,17 @@ module.exports = {
     compress: true,
     hot: true
         },
+
+        plugins: [
+
+            new HTMLWebpackPlugin({
+                template: path.resolve(__dirname, './src/index.html'), 
+                filename: 'index.html',
+                
+              }),
+              new CleanWebpackPlugin(),
+        ]
+
+
     };
 
