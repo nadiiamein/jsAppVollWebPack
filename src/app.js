16b1a45a424f7a7185460d7f775d9286eb80bde6
Nadiia <npodufala15@gmail.com>
2020-12-30 10:32:35 +0200

    js

--- src/app.js
+++ src/app.js
@@ -1,2 +1,9 @@
 import './style.css';
+
+const form =document.getElementById('form');
+const input = form.querySelector('#question-input');
+const submitBtn = form.querySelector('#submit');
+
+form.addEventListener('submit', submitFormHandler);
+
 console.log('App working...');
