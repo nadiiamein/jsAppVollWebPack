afca8e61b6f69bb2c03d4e0d017a2eaef4ce14d6
Nadiia <npodufala15@gmail.com>
2020-12-30 18:36:18 +0200

    submit

--- src/app.js
+++ src/app.js
@@ -1,9 +1,31 @@
+import { isValid } from './utils';
 import './style.css';
+
 
 const form =document.getElementById('form');
 const input = form.querySelector('#question-input');
 const submitBtn = form.querySelector('#submit');
 
 form.addEventListener('submit', submitFormHandler);
+input.addEventListener('input', () => {
+    submitBtn.disabled = !isValid(input.value);
+});
+function submitFormHandler(event) {
+    event.preventDefault()
+    if (isValid (input.value)) {
+     const question = {
+         text: input.value.trim(),
+         date: new Date().toJSON()
+     };
+     submitBtn.disabled = true;
+     //Async request to server to save question    
+     console.log('Question', question);
+
+     input.value = '';
+     input.className = '';
+     submitBtn.disabled = false;
+
+    }
+}
 
 console.log('App working...');
